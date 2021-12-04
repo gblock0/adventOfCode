2a13dfd514604ff809a2b24a656c8d2a2f28ffbe
Greg Block <gblock0@gmail.com>
2021-12-04 11:50:15 -0600

    Move code up a level

--- aoc2020/README.md
+++
@@ -1,1 +0,0 @@
-Learning Rust by completing AoC questions (admittedly not always the most performant)
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod y2020;
+use y2020::day1;
+use y2020::day2;
+pub mod helpers;
+
+fn main() {
+    day1::q1();
+    day1::q2();
+    day2::q1();
+    day2::q2();
+}
